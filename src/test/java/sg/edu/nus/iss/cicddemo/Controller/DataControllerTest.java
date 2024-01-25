8931603dbc59d53f132edcb480dd4b58a0bb4a31
e1327908 <e1327908@u.nus.edu>
2024-01-25 22:07:52 +0800

    clear

---
+++ src/test/java/sg/edu/nus/iss/cicddemo/Controller/DataControllerTest.java
@@ -0,0 +1,24 @@
+package sg.edu.nus.iss.cicddemo.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataControllerTest {
+
+    @Test
+    void healthCheck() {
+    }
+
+    @Test
+    void version() {
+    }
+
+    @Test
+    void getRandomNations() {
+    }
+
+    @Test
+    void getRandomCurrencies() {
+    }
+}
